8cf9fd779cda551a31d14140e6b9d0a51283a9b3
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2025-04-10 13:45:10 +0530

    Delete server directory

--- server/storage/index.ts
+++
@@ -1,5 +0,0 @@
-import type { IStorage } from "./interface";
-import { DatabaseStorage } from "./database";
-
-// Export the storage interface for use in the application
-export const storage: IStorage = new DatabaseStorage();
